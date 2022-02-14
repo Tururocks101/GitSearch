58b82701efc2b7e3e900e8c3ca4b4137e944861c
Tururocks101 <95218150+Tururocks101@users.noreply.github.com>
2022-02-14 08:55:16 +0300

    Add profile

--- github/src/app/app-routing.module.ts
+++ github/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfilecomponentComponent } from './profilecomponent/profilecomponent.component';
+import { NotFoundComponent } from './Error/not-found/not-found.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch:"full", redirectTo:"/home"},
+  {path: "home", component:ProfilecomponentComponent},
+  {path: "404", component:NotFoundComponent},
+  {path: "**", redirectTo:"/404"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
